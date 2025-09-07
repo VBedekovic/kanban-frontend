c30834501eb4d7ac5d6999bf98858f99ce3ba2ca
Vedran Bedeković <vbedekovic16@gmail.com>
2025-09-07 10:35:04 +0200

    added loginm page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TaskBoardView } from './views/task-board-view/task-board-view';
+import { AuthView } from './views/auth-view/auth-view';
 
 export const routes: Routes = [
-    { path: '', component: TaskBoardView }
+    { path: '', component: TaskBoardView },
+    { path: 'auth', component: AuthView }
 ];
---
+++ src/app/views/auth-view/auth-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-view',
+  imports: [],
+  templateUrl: './auth-view.html',
+  styleUrl: './auth-view.css'
+})
+export class AuthView {
+
+}
